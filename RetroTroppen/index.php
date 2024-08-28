52e65a038eb4763543696ecdb6830e154eba07b0
Simon Nyblom <simnyb01@easv365.dk>
2024-08-28 09:35:24 +0200

    Test deployment

---
+++ RetroTroppen/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="test">
+        <h1>Hello World</h1>
+        <h2>Test 123 456 789</h2>
+    </div>
+</body>
+</html>
