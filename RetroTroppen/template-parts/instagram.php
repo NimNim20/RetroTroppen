a08ea706334a458bd01cc44b8ec685aecdca9a4c
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-08-28 13:29:16 +0200

    add headline to instagram

--- RetroTroppen/template-parts/instagram.php
+++ RetroTroppen/template-parts/instagram.php
@@ -9,10 +9,12 @@ $loop = new WP_Query(array(
     <?php while($loop->have_posts()): $loop->the_post() ?>
 
         <?php
+        $headline = get_field("headline");
         $titel = get_field("titel");
         $image = get_field("image");
         $text = get_field("text");
         ?>
+        
 
         <?php echo $titel; ?>
         <img src="<?php echo $image; ?>" alt="">
