b33afd2d3b895f815e444cf69616daac3e93dd32
Simon Nyblom <simnyb01@easv365.dk>
2024-08-28 10:30:02 +0200

    test styling

--- RetroTroppen/functions.php
+++ RetroTroppen/functions.php
@@ -1,16 +1,12 @@
 <?php
 
-
-
-
-
-
 function retro_load_resources() {
     wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
     wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
     //wp_enqueue_script("script", get_template_directory_uri() . "/script.js");
 }
 add_action("wp_enqueue_scripts", "retro_load_resources");
+add_action("wp_enqueue_script")
 
 function portfolio_disable_gutenberg() {
     remove_post_type_support("page", "editor");
