3980582a1113668c86c9bb5bcb5475f704baf803
Benjamin Vid Gard <benvio01@easv365.dk>
2024-09-01 14:34:06 +0200

    Navbar

--- RetroTroppen/header.php
+++ RetroTroppen/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php wp_head() ?>
+    <?php get_template_part("template-parts/navbar") ?>
 </head>
 <body <?php body_class() ?>>
     
---
+++ RetroTroppen/template-parts/navbar.php
@@ -0,0 +1,31 @@
+<?php 
+// Method to check if a user is logged in
+$isLoggedIn = false;
+
+echo '<nav class="navbar">
+    <div class="brand-title">RetroTroppen</div>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+    <div class="navbar-links">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>';
+
+if ($isLoggedIn) {
+  echo '<li><a href="#">Profile</a></li>';
+  echo '<li><a href="#">Logout</a></li>';
+}   else {
+      echo '<li><a href="#">Login</a></li>';
+      echo '<li><a href="#">Register</a></li>';
+}
+
+echo '    </ul>
+  </div>
+</nav>';
+?>
+
+<script src="navigation.js"></script>
