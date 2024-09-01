1129da6128c77549d148fb91fa697893f3271d44
Benjamin Vid Gard <benvio01@easv365.dk>
2024-09-01 14:47:50 +0200

    Navbar testing

--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -1,9 +1,15 @@
 <?php get_header() ?>
+
+<section id="navbar">
+    <?php get_template_part("template-parts/navbar") ?>
+</section>
+
     <div class="test">
         <h1>Hello World</h1>
         <h2>Test 123 456 789</h2>
         <p>Hej</p>
     </div>
+
 <section id="carousel">
     <?php get_template_part("template-parts/carousel") ?>
 </section>
