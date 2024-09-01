b0873e8bbc48061270b4d686baccae5abb21fbd5
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-09-01 11:18:18 +0200

    instragram og index

--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -7,4 +7,19 @@
 <section id="carousel">
     <?php get_template_part("template-parts/carousel") ?>
 </section>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+
+            </div>
+            <div class="col-10">
+                <h1>Nyeste opslag</h1>
+                <?php get_template_part("template-parts/instagram") ?>
+
+                <?php get_template_part("template-parts/articles") ?>
+            </div>
+        </div>
+    </div>
+
 <?php get_footer() ?>
