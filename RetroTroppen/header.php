3ce1b66046a1d41e8a3d5f5c4bd0b8d2860b1907
Benjamin Vid Gard <benvio01@easv365.dk>
2024-09-01 16:44:16 +0200

    Fixed double navbar

--- RetroTroppen/header.php
+++ RetroTroppen/header.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php wp_head() ?>
+
+<section id="navbar">
     <?php get_template_part("template-parts/navbar") ?>
+</section>
+
 </head>
 <body <?php body_class() ?>>
     
