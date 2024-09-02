3db7f1d2d253856c3e0d441f87ce616f084e7e79
Benjamin Vid Gard <benvio01@easv365.dk>
2024-09-02 21:22:22 +0200

    Fixed incoming changes

--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -8,13 +8,6 @@
                     <?php get_template_part("template-parts/navbar") ?>
                 </section>
 
-<<<<<<< HEAD
-    <div class="test">
-        <h1>Hello World</h1>
-        <h2>Test 123 456 789</h2>
-        <p>Hej</p>
-    </div>
-=======
                 <div class="col-12">
                     <?php
                     $Heading = get_field("heading");
@@ -24,7 +17,6 @@
                     <h1><?php echo $Heading; ?></h1>
                     <img src="<?php echo $HeroImage; ?>" alt="">
                 </div>
->>>>>>> 94babab68af2c70f3e49c68bf3bc9c48aac6f701
 
 
                 <section id="carousel">
