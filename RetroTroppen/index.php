717eba13b088f601fb1aa411d3e15d873528294e
idaballegaard <idabal01@easv365.dk>
2024-09-02 12:00:10 +0200

    Hero-image

--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -2,15 +2,30 @@
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
         
-        <section id="navbar">
-            <?php get_template_part("template-parts/navbar") ?>
-        </section>
+        <div class="container">
+            <div class="row">
+                <section id="navbar">
+                    <?php get_template_part("template-parts/navbar") ?>
+                </section>
 
-        <section id="carousel">
-            <?php get_template_part("template-parts/carousel") ?>
-        </section>
+                <div class="col-12">
+                    <?php
+                    $Heading = get_field("heading");
+                    $HeroImage = get_field("hero-image");
+                    ?>
 
-        <?php echo do_shortcode('[cf7form cf7key="contact-form-1"]') ?>
+                    <h1><?php echo $Heading; ?></h1>
+                    <img src="<?php echo $HeroImage; ?>" alt="">
+                </div>
+
+
+                <section id="carousel">
+                    <?php get_template_part("template-parts/carousel") ?>
+                </section>
+
+                <?php echo do_shortcode('[cf7form cf7key="contact-form-1"]') ?>
+            </div>
+        </div>
 
         <?php endwhile; ?>
     <?php endif; ?>
