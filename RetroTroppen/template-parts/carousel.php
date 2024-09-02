40a8d4474d89bb99e50e748dc4471cd58df1d476
Simon Nyblom <simnyb01@easv365.dk>
2024-09-02 21:41:38 +0200

    Carousel fix

--- RetroTroppen/template-parts/carousel.php
+++ RetroTroppen/template-parts/carousel.php
@@ -1,36 +1,36 @@
-    <?php 
-    $image = get_field("carimage1");
-    $image2 = get_field("carimage2");
-    $image3 = get_field("carimage3");
-    ?>
+<?php 
+$image = get_field("carimage1");
+$image2 = get_field("carimage2");
+$image3 = get_field("carimage3");
+?>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <!-- First Slide -->
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-        </div>
-        <!-- Second Slide -->
-        <div class="carousel-item">
-          <img class="d-block w-100" src="<?php echo $image2['url']; ?>" alt="<?php echo $image2['alt']; ?>">
-        </div>
-        <!-- Third Slide -->
-        <div class="carousel-item">
-          <img class="d-block w-100" src="<?php echo $image3['url']; ?>" alt="<?php echo $image3['alt']; ?>">
-        </div>
-      </div>
-
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <!-- First Slide -->
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
     </div>
+    <!-- Second Slide -->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>">
+    </div>
+    <!-- Third Slide -->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo esc_url($image3['url']); ?>" alt="<?php echo esc_attr($image3['alt']); ?>">
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
