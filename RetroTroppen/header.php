d07a801c2183e6464e5c47c33d4499ef6fcfcd78
NimNim20 <benvio01@easv365.dk>
2024-09-02 09:19:20 +0200

    Trying to fix website

--- RetroTroppen/header.php
+++ RetroTroppen/header.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>RetroTroppen</title>
     <?php wp_head() ?>
 
-<section id="navbar">
-    <?php get_template_part("template-parts/navbar") ?>
-</section>
 
 </head>
 <body <?php body_class() ?>>
--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -1,8 +1,21 @@
 <?php get_header() ?>
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post(); ?>
+        
+        <section id="navbar">
+            <?php get_template_part("template-parts/navbar") ?>
+        </section>
 
-<section id="navbar">
-    <?php get_template_part("template-parts/navbar") ?>
-</section>
+        <section id="carousel">
+            <?php get_template_part("template-parts/carousel") ?>
+        </section>
+
+        <section id="instagram">
+            <?php get_template_part("template-parts/instagram") ?>
+        </section>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
 
     <div class="test">
         <h1>Hello World</h1>
@@ -10,12 +23,6 @@
         <p>Hej</p>
     </div>
 
-<section id="carousel">
-    <?php get_template_part("template-parts/carousel") ?>
-</section>
 
-<section id="instagram">
-    <?php get_template_part("template-parts/instagram") ?>
-</section>
 
 <?php get_footer() ?>
