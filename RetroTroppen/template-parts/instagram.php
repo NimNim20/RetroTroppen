1304e2b72722a618af51ef735fea9071b5af771a
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-09-03 09:26:32 +0200

    style img size

--- RetroTroppen/template-parts/instagram.php
+++ RetroTroppen/template-parts/instagram.php
@@ -17,9 +17,8 @@ $loop = new WP_Query(array(
         $text = get_field("text");
         ?>
 
-        <h2 class="header_instagram">Følg os på Sociale media</h2>
         
-        <img src="<?php echo $image["url"]; ?>" alt="<?php echo $image["alt"]; ?>">
+        <img class="instagram_img" src="<?php echo $image["url"]; ?>" alt="<?php echo $image["alt"]; ?>">
 
         <div class="titel_text_instagram">
             <h3><?php echo $titel; ?></h3>
