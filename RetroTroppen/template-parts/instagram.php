76beec86607b2a4a2f38e64c3079906216c8fe83
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-09-03 11:29:07 +0200

    style  instagram text

--- RetroTroppen/template-parts/instagram.php
+++ RetroTroppen/template-parts/instagram.php
@@ -28,7 +28,7 @@ $loop = new WP_Query(array(
             <div class="instagram_img" style="background-image: url(<?php echo $image["url"] ?>);">
 
                 <div class="titel_text_instagram">
-                    <h3 class="insta_titel"><?php echo $titel; ?></h3>
+                    <h4 class="insta_titel"><?php echo $titel; ?></h4>
                     <p class="insta_text"><?php echo $text; ?> </p>
                 </div> 
 
