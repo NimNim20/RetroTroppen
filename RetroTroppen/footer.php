dffa017e1bc1126efa4b379b13304688ce454e0a
Simon Nyblom <simnyb01@easv365.dk>
2024-09-18 12:23:03 +0200

    Footer updated

--- RetroTroppen/footer.php
+++ RetroTroppen/footer.php
@@ -1,16 +1,39 @@
-</main>
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="text-uppercase">About Us</h5>
+          <p>
+            Vores værdier er baseret på kvalitet, design og bæredygtighed. Vi er stolte af at kunne tilbyde vores kunder unikke produkter, som er produceret med omtanke for miljøet.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="text-uppercase">Links</h5>
+          <ul class="list-unstyled mb-0">
+            <li><a href="#" class="text-dark">Home</a></li>
+            <li><a href="#" class="text-dark">About</a></li>
+            <li><a href="#" class="text-dark">Services</a></li>
+            <li><a href="#" class="text-dark">Contact</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="text-uppercase">Follow Us</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-dark">Facebook</a></li>
+            <li><a href="#" class="text-dark">Twitter</a></li>
+            <li><a href="#" class="text-dark">Instagram</a></li>
+            <li><a href="#" class="text-dark">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3 bg-light">
+      © 2024 RetroTroppen:
+      <a class="text-dark" href="#">https://wptest.cmsedu.dk/</a>
+    </div>
+  </footer>
 
+  <?php wp_footer(); ?>
 
-
-
-
-
-
-
-
-
-
-
-     <?php wp_footer(); ?>
-</body>
+  </body>
 </html>
