18e367a7c579064c7f4cdf65b3063c8755c608cb
Simon Nyblom <simnyb01@easv365.dk>
2024-09-18 13:06:08 +0200

    Footer adjustment

--- RetroTroppen/footer.php
+++ RetroTroppen/footer.php
@@ -1,45 +1,48 @@
 <footer class="bg-light text-center text-lg-start">
-    <div class="container p-4">
-      <div class="row" id="footerRow">
-        <div class="col-lg-6 col-md-12 mb-4">
-          <h5 class="text-uppercase">Om os</h5>
-          <p>
-            Vores værdier er baseret på kvalitet, design og bæredygtighed. Vi er stolte af at kunne tilbyde vores kunder unikke produkter og retro møbler.
-          </p>
-        </div>
-        <section id="voresLinks">
-        <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="text-uppercase">Vores Links</h5>
-          <ul class="list-unstyled mb-0">
-            <li><a href="#" class="text-white">Hjem</a></li>
-            <li><a href="#" class="text-white">Om os</a></li>
-            <li><a href="#" class="text-white">Ydelser</a></li>
-            <li><a href="#" class="text-white">Kontakt</a></li>
-          </ul>
-        </div>
-        </section>
-     <section id="følgOs">
-        <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="text-uppercase">Følg os</h5>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">Facebook</a></li>
-            <li><a href="#" class="text-white">Twitter</a></li>
-            <li><a href="#" class="text-white">Instagram</a></li>
-            <li><a href="#" class="text-white">LinkedIn</a></li>
-     </section>
-          </ul>
-        </div>
+  <div class="container p-4">
+    <div class="row" id="footerRow">
+      <!-- Om os Section -->
+      <div class="col-lg-6 col-md-12 mb-4">
+        <h5 class="text-uppercase">Om os</h5>
+        <p>
+          Vores værdier er baseret på kvalitet, design og bæredygtighed. Vi er stolte af at kunne tilbyde vores kunder unikke produkter og retro møbler.
+        </p>
+      </div>
+
+      <!-- Vores Links Section -->
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h5 class="text-uppercase">Vores Links</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="#" class="text-white">Hjem</a></li>
+          <li><a href="#" class="text-white">Om os</a></li>
+          <li><a href="#" class="text-white">Ydelser</a></li>
+          <li><a href="#" class="text-white">Kontakt</a></li>
+        </ul>
+      </div>
+
+      <!-- Følg os Section -->
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h5 class="text-uppercase">Følg os</h5>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-white">Facebook</a></li>
+          <li><a href="#" class="text-white">Twitter</a></li>
+          <li><a href="#" class="text-white">Instagram</a></li>
+          <li><a href="#" class="text-white">LinkedIn</a></li>
+        </ul>
       </div>
     </div>
-    <section id="copyRight">
+  </div>
+
+  <!-- Copyright Section -->
+  <section id="copyRight">
     <div class="text-center p-3 bg-light">
       <p class="text-white">© 2024 RetroTroppen:</p>
       <a class="text-white" href="#">https://wptest.cmsedu.dk/</a>
     </div>
-    </section>
-  </footer>
+  </section>
+</footer>
 
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-  </body>
+</body>
 </html>
