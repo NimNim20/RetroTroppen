55e51e39aad654d13b0dca6e31bbf06c22c0ad60
Simon Nyblom <simnyb01@easv365.dk>
2024-09-18 12:29:04 +0200

    Updated Footer PHP

--- RetroTroppen/footer.php
+++ RetroTroppen/footer.php
@@ -1,21 +1,24 @@
-<footer class="bg-light text-center text-lg-start">
+<footer class="text-center text-lg-start">
     <div class="container p-4">
       <div class="row">
         <div class="col-lg-6 col-md-12 mb-4">
-          <h5 class="text-uppercase">About Us</h5>
+          <h5 class="text-uppercase">Om os</h5>
           <p>
-            Vores værdier er baseret på kvalitet, design og bæredygtighed. Vi er stolte af at kunne tilbyde vores kunder unikke produkter, som er produceret med omtanke for miljøet.
+            Vores værdier er baseret på kvalitet, design og bæredygtighed. Vi er stolte af at kunne tilbyde vores kunder unikke produkter og retro møbler.
           </p>
         </div>
+        <section id="voresLinks">
         <div class="col-lg-3 col-md-6 mb-4">
           <h5 class="text-uppercase">Vores Links</h5>
           <ul class="list-unstyled mb-0">
             <li><a href="#" class="text-dark">Hjem</a></li>
             <li><a href="#" class="text-dark">Om os</a></li>
             <li><a href="#" class="text-dark">Ydelser</a></li>
-            <li><a href="#" class="text-dark">Kontak</a></li>
+            <li><a href="#" class="text-dark">Kontakt</a></li>
           </ul>
         </div>
+        </section>
+     <section id="følgOs">
         <div class="col-lg-3 col-md-6 mb-4">
           <h5 class="text-uppercase">Følg os</h5>
           <ul class="list-unstyled">
@@ -23,14 +26,17 @@
             <li><a href="#" class="text-dark">Twitter</a></li>
             <li><a href="#" class="text-dark">Instagram</a></li>
             <li><a href="#" class="text-dark">LinkedIn</a></li>
+     </section>
           </ul>
         </div>
       </div>
     </div>
+    <section id="copyRight">
     <div class="text-center p-3 bg-light">
       © 2024 RetroTroppen:
       <a class="text-dark" href="#">https://wptest.cmsedu.dk/</a>
     </div>
+    </section>
   </footer>
 
   <?php wp_footer(); ?>
