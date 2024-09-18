5fe4ab96ce2d68f5754357da0c3bcfa3c1ac7c2a
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-09-18 13:06:20 +0200

    inst

--- RetroTroppen/template-parts/instagram.php
+++ RetroTroppen/template-parts/instagram.php
@@ -8,6 +8,9 @@ $loop = new WP_Query(array(
 <h2 class="header_instagram">
 <?php pll_e("Følg os på Sociale media")?></h2>
 
+<div class="container">
+<div class="row">
+
 <div class="instagram_boxes">
     <?php if($loop->have_posts()): ?>
         <?php while($loop->have_posts()): $loop->the_post() ?>
@@ -20,11 +23,10 @@ $loop = new WP_Query(array(
             ?>
 
 
-            <div class="container">
-                <div class="row">
+           
                     <div class="col-3">
                         <div class="card ">
-                            
+
                             <img src="url(<?php echo $image["url"] ?>)" class="card-img" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $titel; ?></h5>
@@ -35,11 +37,14 @@ $loop = new WP_Query(array(
 
                     </div>
                 
-                </div>
-            </div>
+           
             
         
 
         <?php endwhile; ?>
     <?php endif; ?>
+
+
+    </div>
+    </div>
 </div>
