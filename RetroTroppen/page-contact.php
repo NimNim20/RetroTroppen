0fddfb92a105257e1a3ad3c052f1553b79e696f6
NimNim20 <benvio01@easv365.dk>
2024-09-18 14:09:33 +0200

    Contact form edits

--- RetroTroppen/page-contact.php
+++ RetroTroppen/page-contact.php
@@ -3,17 +3,7 @@
         <?php while(have_posts()): the_post(); ?>
 
 
-
-
-
-
- <?php echo do_shortcode('[cf7form cf7key="contact-form-1"]') ?>
-
-
-
-
-
-
+            <?php echo do_shortcode('[cf7form cf7key="contact-form-1"]') ?>
 
 
         <?php endwhile; ?>
