9a16d9faf0732caafc3e8cbde7048784e29f674b
NimNim20 <benvio01@easv365.dk>
2024-09-25 13:56:43 +0200

    Navbar dropdown icon

--- RetroTroppen/template-parts/navbar.php
+++ RetroTroppen/template-parts/navbar.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <i class="bi bi-list"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -22,8 +22,8 @@
           <a class="nav-link" href="<?php echo site_url('/contact')?>">Kontakt</a>
         </li>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Søg..." aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Søg</button>
+        <input class="form-control me-2 search" type="search" placeholder="Søg..." aria-label="Search">
+        <button class="btn btn-outline-success search-btn" type="submit">Søg</button>
       </form>
     </div>
   </div>
