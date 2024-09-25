b6c285e8c861610aa123ae1ee649d3ea3c4ad940
idaballegaard <idabal01@easv365.dk>
2024-09-25 13:38:04 +0200

    deleted container-fluid in index

--- RetroTroppen/index.php
+++ RetroTroppen/index.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
     
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <!-- Main Content Section -->
             <main>
