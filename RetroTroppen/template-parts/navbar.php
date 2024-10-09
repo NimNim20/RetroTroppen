f61f49112468dbf1b39223d576506a8e1e14aba7
Simon Nyblom <simnyb01@easv365.dk>
2024-10-09 09:40:55 +0200

    Navbar check

--- RetroTroppen/template-parts/navbar.php
+++ RetroTroppen/template-parts/navbar.php
@@ -1,9 +1,27 @@
-<nav class="navbar navbar-light bg-light">
-        <div class="container-fluid justify-content-center">
-            <form class="d-flex w-100 justify-content-center">
-                <input class="form-control me-2 navbar-search" type="search" placeholder="Search" aria-label="Search">
+
+    <!-- Navbar with Logo, Search Bar, Cart, and User Icon -->
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <!-- Logo on the left -->
+            <a class="navbar-brand" href="#">
+                <img src="https://via.placeholder.com/40x40" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
+            </a>
+            
+            <!-- Search bar in the center -->
+            <form class="d-flex mx-auto navbar-search">
+                <input class="form-control me-2 w-100" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Søg</button>
             </form>
+            
+            <!-- Cart and User icons on the right -->
+            <div class="d-flex">
+                <a href="#" class="me-3">
+                    <img src="https://via.placeholder.com/24x24" alt="Cart Icon" width="24" height="24">
+                </a>
+                <a href="#">
+                    <img src="https://via.placeholder.com/24x24" alt="User Icon" width="24" height="24">
+                </a>
+            </div>
         </div>
     </nav>
 
