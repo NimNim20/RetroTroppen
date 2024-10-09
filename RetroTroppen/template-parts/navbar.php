afc2905ca3979cf2b99536ff86898e483fd41d22
Simon Nyblom <simnyb01@easv365.dk>
2024-10-09 09:39:07 +0200

    Testing of new Navbar

--- RetroTroppen/template-parts/navbar.php
+++ RetroTroppen/template-parts/navbar.php
@@ -1,30 +1,34 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Forside</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo site_url('/blog')?>">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Web Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Butik</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo site_url('/contact')?>">Kontakt</a>
-        </li>
-      <form class="d-flex" role="search">
-        <input id="search-area" class="form-control me-2 search" type="search" placeholder="Søg..." aria-label="Search">
-        <button class="btn btn-outline-success search-btn" type="submit">Søg</button>
-      </form>
-    </div>
-  </div>
-</nav>
+<nav class="navbar navbar-light bg-light">
+        <div class="container-fluid justify-content-center">
+            <form class="d-flex w-100 justify-content-center">
+                <input class="form-control me-2 navbar-search" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Søg</button>
+            </form>
+        </div>
+    </nav>
+
+    <!-- Navbar with Links -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Web shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Butik</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakt</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Søg</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
