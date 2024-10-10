94f8cb179c4aeaf2f5766f340b5fbe647df2d28a
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-10-10 22:37:24 +0200

    lang

--- RetroTroppen/header.php
+++ RetroTroppen/header.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <div class="outlang">
-        <div class="lang"> 
-            <?php pll_the_languages() ?>
-        </div>
-    </div>
     <title><?php pll_e("RetroTroppen")?></title>
     <?php wp_head() ?>
             
