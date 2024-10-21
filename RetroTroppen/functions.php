9fd81df4c5e4d127669eef05c364a73481e9cc0c
Simon Nyblom <simnyb01@easv365.dk>
2024-10-21 10:16:44 +0200

    Added the link

--- RetroTroppen/functions.php
+++ RetroTroppen/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function retro_load_resources() {
+    wp_enqueue_style("woocommerce", get_template_directory_uri() . "/WooCommerce.css");
     wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
     wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array(), false, true);
     wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
