67219684ba11b079662a19e9e4d85f5597be4ef4
idaballegaard <idabal01@easv365.dk>
2024-10-21 13:14:37 +0200

    Keyword på instaposts

--- RetroTroppen/template-parts/instagram.php
+++ RetroTroppen/template-parts/instagram.php
@@ -6,7 +6,7 @@ $loop = new WP_Query(array(
 
 
 <h2 class="header_instagram">
-<?php pll_e("Følg os på Sociale media")?></h2>
+<?php pll_e("RetroTroppen på Instagram")?></h2>
 
 <div class="container">
 <div class="row">
@@ -21,30 +21,21 @@ $loop = new WP_Query(array(
             $text = get_field("text");
             $date = get_field("date")
             ?>
-
-
            
-                <div class="col-3">
-                    <div class="card h-100 d-flex flex-column"> <!-- Ensure card takes full height -->
-                        <img src="<?php echo $image["url"] ?>" class="card-img-top" alt="...">
-        
-                        <div class="card-body d-flex flex-column"> <!-- Make the body stretch with flex -->
-                            <h5 class="card-title"><?php echo $titel; ?></h5>
-                                <p class="card-text"><?php echo $text; ?></p>
-                                    <p class="card-text mt-auto"><small class="text-body-secondary"><?php echo $date; ?></small></p>
-                        </div>
+            <div class="col-3">
+                <div class="card h-100 d-flex flex-column"> <!-- Ensure card takes full height -->
+                    <img src="<?php echo $image["url"] ?>" class="card-img-top" alt="...">
+    
+                    <div class="card-body d-flex flex-column"> <!-- Make the body stretch with flex -->
+                        <h5 class="card-title"><?php echo $titel; ?></h5>
+                        <p class="card-text"><?php echo $text; ?></p>
+                        <p class="card-text mt-auto"><small class="text-body-secondary"><?php echo $date; ?></small></p>
                     </div>
                 </div>
-
-                
-           
-            
-        
+            </div>
 
         <?php endwhile; ?>
     <?php endif; ?>
-
-
-    
+  
     </div>
 </div>
